44176be3e51565cfc80a179460204b83493cd532
Jack Kingsman <jack.kingsman@gmail.com>
2014-12-03 02:55:43 -0500

    First real commit

--- twilio-sms-delete.js
+++ twilio-sms-delete.js
@@ -1,16 +1,60 @@
 #!/usr/bin/env node
-
-console.log('hello world');
-
 var program =   require('commander');
-var twilio =    require('twilio');
+var assert =    require('assert');
 
 program
-    .version(pkg.version)
-    .option('-s, --sid <sid>',          'Your Twilio API Account SID')
-    .option('-a, --auth <authtoken>',   'Your Twilio API Auth Token')
-    .option('-v, --verbose',            'Display extra information')
-    .option('-d, --dryrun',             'Doesn\'t make any changes to actual data, but displays what it would do')
+    .option('--sid <sid>',          'Your Twilio API Account SID')
+    .option('--auth <authtoken>',   'Your Twilio API Auth Token')
     .parse(process.argv);
+
+
+assert.ok(program.sid, "SID is required")
+assert.ok(program.auth, "Auth Token is required")
+
+
+//build our client
+var client = require('twilio')(program.sid, program.auth);
+
+
+client.messages.list({}, function(err, data) {
+    if (err) {
+        console.log(err);
+        console.log("This usually means your credentials are bad.");
+        process.exit(1);
+    }
     
-console.log("Initializing with SID " + program.sid + " and auth token " + program.authtoken);
+    //loop through every page
+    for(var currPage = 0; currPage < data.num_pages; currPage++) {
+        //retrieve the current page
+        client.messages.list({Page: currPage}, function(err, data) {
+            //get a list of messages for the page (currPage)
+            data.messages.forEach(function(message) {
+                if (message.num_media != 0) {
+                    //this message has media; retrive it
+                    client.messages(message.sid).media.list(function(err, data) {
+                        //pull each peice of media from the message
+                        data.mediaList.forEach(function(media) {
+                            //delete each peice of media
+                            client.messages(media.parent_sid).media(media.sid).delete(function(err, data) {
+                                if (err) {
+                                    console.log("Media deletion error: " + err.status + " -- " + err.message);
+                                } else {
+                                    console.log("Deleted Media SID " + media.sid + " successfully.");
+                                }
+                            });
+                        });
+                    });
+                }
+                
+                //delete the message itself
+                client.messages(message.sid).delete(function(err, data) {
+                    if (err) {
+                        console.log("Message deletion error: " + err.status + " -- " + err.message);
+                    } else {
+                        console.log("Deleted Message SID " + message.sid + " successfully.");
+                    }
+                });
+            }); 
+        });
+    }
+});
